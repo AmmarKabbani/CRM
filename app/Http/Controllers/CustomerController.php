6fe1168025d47fb024e32cb918f8ac685c23aac8
AmmarKabbani <3mmar.kabbani@gmail.com>
2023-03-08 06:47:57 +0300

    add 2 new APIs ( Customer_info , Customers_info  )
    get all info of specific user by his id
    and
    get all customers

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
-    //
+    /**
+     * get all info of specific user by his id
+     * @param  int $CustomerId
+     */
+    public function Customer_info(int $CustomerId){
+        $info = Customer::where('id' , $CustomerId)->get();
+        //check if $info has a value 
+        if(json_decode($info , true)){
+            return $this->success($info,'Customer information');
+        }
+        else{
+            return $this->success('','Customer not found !');
+        }
+    }
+
+    public function Customers_info(){
+        $info = Customer::all();
+            return $this->success($info,'Customer information');
+    }
 }
