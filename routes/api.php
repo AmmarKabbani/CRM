b19ad813bca4fc34a29ffb6dceca312bd39af4ed
AmmarKabbani <3mmar.kabbani@gmail.com>
2023-03-08 06:51:02 +0300

    add routes for APIs
    for { register - login - Customer_info - Customers_info

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CustomerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'api'], function ($routes) {
+    Route::post('/registerCustomer',[AuthController::class , "register"]);
+    Route::post('/login',[AuthController::class , "login"]);
+    Route::get('/customerInfo/customerId={id}',[CustomerController::class , "Customer_info"]);
+    Route::get('/customersInfo',[CustomerController::class , "Customers_info"]);
 });
